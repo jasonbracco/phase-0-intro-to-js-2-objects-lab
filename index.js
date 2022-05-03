f7d762e28e4479c9e8352d9610e97cae42461448
Jason Bracco <bracco.jason@gmail.com>
2022-05-02 20:56:14 -0400

    commit tree fdc998da80f95142c9f1af200d7f54a19f1cc5e1

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Jason Bracco",
+    address: "1268 Columbia Road",
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {...obj, [key]:value,}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete employee[key]
+    return employee
+}
+
+
